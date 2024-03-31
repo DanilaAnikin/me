947b685a38927ccc37ab75df00398af7d68d9341
DanilaAnikin <danakin10502@gmail.com>
2024-03-31 17:01:18 +0200

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <span class="text-slate-900">
+            Hey, I'm Danila Anikin
+        </span>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full">
+        <Navbar />
+    </div>
+</template>
+
+<style>
+body {
+    background-color: #fffbeb;
+}
+</style>
